bfe09a3f3054f44f835ce7e0e9a8934ad09cde8b
GustavoRutkowski <rutkowskigustavo@gmail.com>
2025-10-04 03:31:45 -0300

    Create 404 and 418 custom pages

--- frontend/server.js
+++ frontend/server.js
@@ -11,33 +11,47 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'index.html'));
 });
 
-app.get('/faq', (req, res) => {
+// Alias
+app.get('/index', (_, res) => res.redirect('/'));
+
+app.get('/faq', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'faq.html'));
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'login.html'));
 });
 
-app.get('/register', (req, res) => {
+app.get('/register', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'register.html'));
 });
 
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'dashboard.html'));
 });
 
-app.get('/user-settings', (req, res) => {
+app.get('/user-settings', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'user-settings.html'));
 });
 
-app.get('/admin-panel', (req, res) => {
+app.get('/admin-panel', (_, res) => {
     res.sendFile(path.join(__dirname, 'view', 'admin-panel.html'));
 });
+
+app.get('/404', (_, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'view', '404.html'));
+});
+
+app.get('/418', (_, res) => {
+    res.status(418).sendFile(path.join(__dirname, 'view', '418.html'));
+});
+
+app.get('/teapot', (_, res) => res.redirect('/418'));
+app.use((_, res) => res.redirect('/404'));
 
 const PORT = process.env.SITE_PORT || 5500;
 
